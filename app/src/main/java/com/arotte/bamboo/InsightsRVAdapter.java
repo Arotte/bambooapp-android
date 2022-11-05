477e9f56898e22a85469cde4a9176d0ec0b50ba9
Aron Molnar <thearote@gmail.com>
2022-11-05 15:11:35 +0000

    fix bamboo

--- app/src/main/java/com/arotte/bamboo/InsightsRVAdapter.java
+++ app/src/main/java/com/arotte/bamboo/InsightsRVAdapter.java
@@ -1,6 +1,7 @@
 package com.arotte.bamboo;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,9 +12,34 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
 public class InsightsRVAdapter extends RecyclerView.Adapter<InsightsRVAdapter.ViewHolder> {
+
+
+
+    // variable for our bar chart
+    // BarChart barChart;
+
+    // variable for our bar data.
+    BarData barData;
+
+    // variable for our bar data set.
+    BarDataSet barDataSet;
+
+    // array list for storing entries.
+    ArrayList barEntriesArrayList;
+
+
+
+
 
     private final List<ViewStats> data;
     private final LayoutInflater mInflater;
@@ -39,6 +65,38 @@ public class InsightsRVAdapter extends RecyclerView.Adapter<InsightsRVAdapter.Vi
 
         // set recipe name
         holder.tvAppName.setText(appName);
+
+        // -------------------------------------------------------------
+        // barchart
+
+//
+// initializing variable for bar chart.
+//        barChart = findViewById(R.id.idBarChart);
+        // calling method to get bar entries.
+        getBarEntries();
+
+        // creating a new bar data set.
+        barDataSet = new BarDataSet(barEntriesArrayList, "Feelings");
+
+        // creating a new bar data and
+        // passing our bar data set.
+        barData = new BarData(barDataSet);
+
+        // below line is to set data
+        // to our bar chart.
+        holder.barChart.setData(barData);
+
+        // adding color to our bar data set.
+        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+
+        // setting text color.
+        barDataSet.setValueTextColor(Color.BLACK);
+
+        // setting text size
+        barDataSet.setValueTextSize(16f);
+        holder.barChart.getDescription().setEnabled(false);
+
+        // --------------------------------------------------------------------
     }
 
     public String getURLForResource (int resourceId) {
@@ -56,11 +114,12 @@ public class InsightsRVAdapter extends RecyclerView.Adapter<InsightsRVAdapter.Vi
     // stores and recycles views as they are scrolled off screen
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView    tvAppName;
+        BarChart barChart;
 
         ViewHolder(View itemView) {
             super(itemView);
             tvAppName   = itemView.findViewById(R.id.tvAppName);
-
+            barChart = itemView.findViewById(R.id.bc);
 
             itemView.setOnClickListener(this);
         }
@@ -84,5 +143,23 @@ public class InsightsRVAdapter extends RecyclerView.Adapter<InsightsRVAdapter.Vi
     // parent activity will implement this method to respond to click events
     public interface ItemClickListener {
         void onRecItemClick(View view, int position);
+    }
+
+
+    // ==============================================================================================
+    // barchart
+
+    private void getBarEntries() {
+        // creating a new array list
+        barEntriesArrayList = new ArrayList<>();
+
+        // adding new entry to our array list with bar
+        // entry and passing x and y axis value to it.
+        barEntriesArrayList.add(new BarEntry(1f, 4));
+        barEntriesArrayList.add(new BarEntry(2f, 6));
+        barEntriesArrayList.add(new BarEntry(3f, 8));
+        barEntriesArrayList.add(new BarEntry(4f, 2));
+        barEntriesArrayList.add(new BarEntry(5f, 4));
+        barEntriesArrayList.add(new BarEntry(6f, 1));
     }
 }

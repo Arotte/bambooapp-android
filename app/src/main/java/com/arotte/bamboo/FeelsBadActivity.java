020b5fbc7aad873ac46da00b9fd8c8c978cd65be
Aron Molnar <thearote@gmail.com>
2022-11-06 03:24:49 +0000

    demo

--- app/src/main/java/com/arotte/bamboo/FeelsBadActivity.java
+++ app/src/main/java/com/arotte/bamboo/FeelsBadActivity.java
@@ -3,6 +3,8 @@ package com.arotte.bamboo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 public class FeelsBadActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class FeelsBadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feels_bad);
+
+        ((TextView)findViewById(R.id.tvBadFeelingTxt)).setText(Html.fromHtml(
+                "I have generated an image <br/>of a <b>cute doggo sitting<br/>on the moon</b> to<br/>cheer you up!"));
     }
 }

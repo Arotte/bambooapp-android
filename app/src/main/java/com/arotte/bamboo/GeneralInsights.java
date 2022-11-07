b28fc4b957e3dd836a428b543e984d82e5ef1078
Aron Molnar <thearote@gmail.com>
2022-11-07 11:24:47 +0000

    update

---
+++ app/src/main/java/com/arotte/bamboo/GeneralInsights.java
@@ -0,0 +1,14 @@
+package com.arotte.bamboo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GeneralInsights extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_general_insights);
+    }
+}
